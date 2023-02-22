1b40e6c605e1fd0115898bbbb57c05f1f8aa412f
harry <hkjlee109@gmail.com>
2023-02-22 18:52:20 +0100

    Add checkmark

--- kartoffel-music-app/Sources/Features/GoogleDriveFeature/Views/DowloadAccessoryView.swift
+++ kartoffel-music-app/Sources/Features/GoogleDriveFeature/Views/DowloadAccessoryView.swift
@@ -23,7 +23,7 @@ class DownloadAccessoryView: UIView {
     private func render() {
         switch state {
         case .nothing:
-            let circularPath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: 10, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+            let circularPath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2, y: frame.size.height / 2), radius: 10, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
             
             backgroundLayer.path = circularPath.cgPath
             backgroundLayer.fillColor = UIColor.clear.cgColor
@@ -32,13 +32,30 @@ class DownloadAccessoryView: UIView {
             layer.addSublayer(backgroundLayer)
             break
         case .selected:
-            let circularPath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: 10, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+            let circularPath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2, y: frame.size.height / 2), radius: 10, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
             
             backgroundLayer.path = circularPath.cgPath
             backgroundLayer.fillColor = UIColor.theme.secondary.cgColor
             backgroundLayer.lineWidth = 2.0
             backgroundLayer.strokeColor = UIColor.theme.secondary.cgColor
             layer.addSublayer(backgroundLayer)
+            
+            let imageView = UIImageView(
+                image: UIImage(
+                    systemName: "checkmark",
+                    withConfiguration: UIImage.SymbolConfiguration(
+                        pointSize: 12,
+                        weight: .heavy
+                    )
+                )
+            )
+            imageView.tintColor = .theme.background
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(imageView)
+            NSLayoutConstraint.activate([
+                imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+                imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            ])
             break
         case let .downloading(progress):
             break
